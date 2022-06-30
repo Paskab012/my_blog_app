5680e8675cb1a7fde157d4a3e669d4bf779ed63b
Pascal <pascal@Pascals-MacBook-Pro.local>
2022-07-01 00:03:37 +0200

    Correct application_controller file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,13 +11,15 @@ class ApplicationController < ActionController::Base
 
   def all_users_post_controller
     User.find(params[:user_id])
-    protected
+  end
 
-    def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: %i[name photo bio])
-      devise_parameter_sanitizer.permit(:account_update, keys: %i[name photo bio])
-      devise_parameter_sanitizer.permit(:sign_in) do |user_params|
-        user_params.permit(:username, :email)
-      end
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name photo bio])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[name photo bio])
+    devise_parameter_sanitizer.permit(:sign_in) do |user_params|
+      user_params.permit(:username, :email)
     end
+  end
 end
